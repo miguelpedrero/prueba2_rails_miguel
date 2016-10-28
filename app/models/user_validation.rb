38dad7e18ec8d77f68971e5c0a41099f4720d782
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-10-28 20:31:14 -0300

    Diagrama

--- app/models/user_validation.rb
+++ app/models/user_validation.rb
@@ -1,4 +1,5 @@
 class UserValidation < ActiveRecord::Base
+	has_many :item
 	validates :name, :presence => true
 
 	validates :email, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028225109) do
+ActiveRecord::Schema.define(version: 20161028230022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161028225109) do
     t.integer  "size"
     t.text     "description"
     t.integer  "category_id"
+    t.string   "user"
   end
 
   add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
